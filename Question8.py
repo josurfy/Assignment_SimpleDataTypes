b099ef6c12b15f4fff23a309ba213226fec7754b
josurfy <j.taft.dawson@gmail.com>
2025-09-02 23:12:34 -0600

    Create Question8.py

---
+++ Question8.py
@@ -0,0 +1,7 @@
+weight_kg = 60 #assign Susan's weight of 60kg to variable weight_kg
+
+height_m = 1.58 #assign Susan's height of 1.58m to variable height_m 
+
+BMI = weight_kg/height_m**2 #calculate Susan's BMI using cdc equation
+
+print(BMI) #print Susan's BMI

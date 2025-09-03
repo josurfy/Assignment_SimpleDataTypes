cf7de20f17538dec6adbc9a55a863d567a31beee
josurfy <j.taft.dawson@gmail.com>
2025-09-02 23:47:33 -0600

    Create Question11.py

---
+++ Question11.py
@@ -0,0 +1,41 @@
+# Booleans
+# boolean True + boolean True - boolean False =?
+Question1 = True + True - False
+print(Question1)
+
+# Booleans 2
+# logical tracker = True
+# logical tracker = logical tracker & logical tracker
+# logical tracker = logical tracker | (logical tracker - logical tracker)
+# logical tracker = ?
+a = True
+a == a & a
+a == a | (a - a)
+print(a)
+
+# Strings
+# string = "E. Coli"
+# string[2]=
+string = "E. Coli"
+Question5 = string[2]
+print(Question5)
+
+# Strings 2
+# string = "I Bove python"
+# string[2]="L"
+string = "I Bove python"
+print(string)
+new_string = string.replace("B", "L")
+print(new_string)
+
+# Operators 2
+# Explain how the "+", addition operation works differently for integers and strings
+print("Integers: 5 + 5 = 10")
+print("Strings: '5' + '5' = 55")
+
+# Strings 3
+# Type the output if the strings "7" and "3" are added
+print("'7' + '3' = ")
+print('7' + '3')
+
+
